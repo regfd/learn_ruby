23806323bf3667b862221ac130735807085e93da
regfd <neubauersimon@hotmail.com>
2020-01-27 23:05:18 -0500

    Simon Says

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,33 @@
 #write your code here
+def echo(input)
+    input
+end
+
+def shout(input)
+    input.upcase
+end
+
+def repeat(input, x=2)
+    input + (" #{input}" * (x-1))
+end
+
+def start_of_word(input, value)
+    input[0..value-1]
+end
+
+def first_word(input)
+    r = input.split
+    r[0]
+end
+
+def titleize(s)
+    words = s.split.map{ |word|
+      if %w(the and over).include?(word)
+        word
+      else
+        word.capitalize
+      end
+    }
+    words.first.capitalize!
+    words.join(" ")
+end
